84a3ecc6b31d523908ddfbf30f05ed60c21594d1
anthony-97 <79821814+anthony-97@users.noreply.github.com>
2021-04-23 01:19:05 -0600

    Add files via upload

--- Medicamento.py
+++ Medicamento.py
@@ -1,19 +1,19 @@
 #Nombre: texto
 #Precio: decimal
-# Descripción: texto 
+#Descripción: texto 
 #Cantidad: numero 
 
-class  Medicamento():
-    def  __init__ ( self , nombre , precio , descripcion , cantidad ):
-        self.nombre  =  nombre
-        self.precio  =  precio
-        self.descripcion  =  descripcion
-        self.cantidad  =  cantidad
+class Medicamento():
+    def __init__(self,nombre,precio,descripcion,cantidad):
+        self.nombre = nombre
+        self.precio = precio
+        self.descripcion = descripcion
+        self.cantidad = cantidad
     
-    def  get_json (self):
+    def get_json(self):
         return {
-            "nombre" : self.nombre ,
-            "precio" : self.precio ,
-            "descripcion" : self.descripcion ,
-            "cantidad" : self.cantidad
+            "nombre" : self.nombre,
+            "precio" : self.precio,
+            "descripcion" : self.descripcion,
+            "cantidad": self.cantidad
         }
